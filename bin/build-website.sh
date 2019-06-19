8e52b072b8a6a097244e00bbe2cea4c2295b45db
Nickolas Da Silva <nickolas.silva@researchgate.net>
2019-06-19 19:18:43 +0200

    Update site build script

--- bin/build-website.sh
+++ bin/build-website.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-echo "[Website] inserir script the build aqui..."
+set -e
+source .env_build
+if [ "$ENV" != "BUILD" ]; then source .env_dev; fi;
+
+# Make "site/" current working dir
+cd site/
+
+composer install
+yarn install
+
+yarn run $JIGSAW_ENV
+
+# Moves build results to docs/ so it gets presented on Github Pages
+mv build_$JIGSAW_ENV/* ../docs/
