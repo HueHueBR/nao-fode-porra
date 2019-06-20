8fbabee2713c26af4dfd42edc88fe46cefd2f80b
Níckolas Daniel da Silva <nickolas@phpsp.org.br>
2019-06-20 10:32:39 +0200

    Adding multiple build steps and two envs: php + python (#27)

---
+++ bin/s3/download-archive.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+source .env_build
+if [ "$ENV" != "BUILD" ]; then source .env_dev; fi;
+
+ARCHIVE=$1
+DATE=$2
+
+if [ -z "$DATE" ]; then
+  DATE=$(date +%F);
+fi;
+
+S3_BUCKET=$BUILD_S3_UPLOAD_BUCKET
+URL=$S3_REST_BASE_URL/$S3_BUCKET/archive_$DATE\_$ARCHIVE
+
+curl -L $URL -X GET
